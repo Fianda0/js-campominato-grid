84213171e85e86f379525d64a19596d9b08977cb
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-30 16:32:29 +0200

    Bonus 3

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+
+
+//Bottone per iniziare
 document.getElementById('btn-play').addEventListener('click', function () {
 
-    //Aggiungo dysplay none al click del bottone
+    //Recupero il valore del livello
+    let livello = document.getElementById('difficolta')
+    let valore = livello.value;
+
+    let nCelle = 100;
+
+    //Aggiungo dysplay none al click del bottone e selezione
     document.getElementById('btn-play').classList.add('d-none')
+    document.getElementById('difficolta').classList.add('d-none')
 
     let grid = document.getElementById('griglia')
 
+    //in base alla dificoltà scelta cambia la griglia
+    if (valore == 'Medio') {
+        nCelle = 81
+        grid.classList.add('grid-medio')
+    } else if (valore == 'Difficile') {
+        nCelle = 49
+        grid.classList.add('grid-difficile')
+    } else {
+        grid.classList.add('grid-facile')
+
+    }
+
+
     //Creo ciclo for per creare i quadrati
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= nCelle; i++) {
 
         //Richiamo funzione CREAQUADRATO
         let quadrato = creaQuadrato(i)
